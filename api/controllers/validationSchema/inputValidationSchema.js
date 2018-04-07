d9e592003c8bfbef94427322abe32ca2a8952768
Alexandre Resende <aar6947@gmail.com>
2018-04-07 15:07:05 -0300

    feature: Implement getBarCodeValue

--- api/controllers/validationSchema/inputValidationSchema.js
+++ api/controllers/validationSchema/inputValidationSchema.js
@@ -2,7 +2,7 @@
 const Joi = require('joi');
 
 const inputValidationSchema = {
-  barCodeNumber: Joi.string().min(44).max(46).required(),
+  barCodeNumber: Joi.string().min(47).max(48).required(),
 };
 
 module.exports = inputValidationSchema;
---
+++ test/getBarCodeValue.test.js
@@ -0,0 +1,40 @@
+
+const chai = require('chai');
+const roundTo = require('round-to');
+const expect = chai.expect;
+
+const getBarCodeValue = require('../api/helper/getBarCodeValue');
+
+describe(`Test getBarCodeValue module`, () => {
+  let barCodeNumber;
+
+  describe(`with dealership barcode numbers`, () => {
+    beforeEach((done) => {
+      barCodeNumber = '846700000009799001090114001512793801300995016940';
+      done();
+    });
+  
+    it(`should return value 79.90`, (done) => {
+      const barCodeValue = getBarCodeValue(barCodeNumber);
+      const expectedValue = 79.90;
+  
+      expect(roundTo(barCodeValue, 2)).to.equal(expectedValue);
+      done();
+    });
+  });
+
+  describe(`with bank barcode numbers`, () => {
+    beforeEach((done) => {
+      barCodeNumber = '34191760074639344129352618690003372460000360125';
+      done();
+    });
+  
+    it(`should return value 3601.25`, (done) => {
+      const barCodeValue = getBarCodeValue(barCodeNumber);
+      const expectedValue = 3601.25;
+
+      expect(roundTo(barCodeValue, 2)).to.equal(expectedValue);
+      done();
+    });
+  });
+});
