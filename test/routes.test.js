373fad3b6a017bbcb77f6d68592163278aa8c465
Alexandre Resende <alexandre.amres@gmail.com>
2018-04-10 12:22:50 -0300

    tests: Increment route tests

--- test/routes.test.js
+++ test/routes.test.js
@@ -9,23 +9,112 @@ const app = require('../app');
 chai.use(chaiHttp);
 
 describe(`Test app routes`, () => {
-  it(`should return a JSON on the POST method`, (done) => {
-    const expectedValue = 79.90;
+  describe(`Testing the POST route /barcodeticket`, () => {
+    let response;
 
-    chai
+    beforeEach((done) => {
+      const endpoint = `/barcodeticket`;
+
+      chai
       .request(app)
-      .post('/barcodeticket')
+      .post(endpoint)
       .set('content-type', 'application/json')
       .send({
         ticketTypedLine: '846700000009799001090114001512793801300995016940',
       })
       .end(function(err, res) {
-        console.log(res.statusCode);
-        console.log(res.body);
+        response = res;
         done();
       });
+    });
 
-    /* expect(roundTo(getTicketValueResult, 2)).to.equal(expectedValue);
-    done(); */
+    it(`should return a status code 200`, (done) => {
+      const expectedValue = 200;
+      expect(response.statusCode).to.be.equal(200);
+      done();      
+    });
+
+    it(`should return an object`, (done) => {
+      const expectedValue = 'object';
+      expect(response.body).to.be.an('object');
+      done();      
+    });
+
+    it(`should contains a ticketData key`, (done) => {
+      const expectedValue = 'ticketData';
+      expect(response.body).to.have.property(expectedValue);
+      done();      
+    });
+
+    it(`should contains a value key`, (done) => {
+      const expectedValue = 'value';
+      expect(response.body.ticketData).to.have.property(expectedValue);
+      done();      
+    });
+
+    it(`should contains a dueDate key`, (done) => {
+      const expectedValue = 'dueDate';
+      expect(response.body.ticketData).to.have.property(expectedValue);
+      done();      
+    });
+
+    it(`should contains a dueDate key`, (done) => {
+      const expectedValue = 'barCode';
+      expect(response.body.ticketData).to.have.property(expectedValue);
+      done();      
+    });
+  });
+
+  describe(`Testing the GET route /barcodeticket`, () => {
+    let response;
+
+    beforeEach((done) => {
+      const endpoint = `/barcodeticket`;
+
+      chai
+      .request(app)
+      .get('/barcodeticket?ticketTypedLine=34191760074639344129352618690003372460000360125')
+      .set('content-type', 'application/json')
+      .end(function(err, res) {
+        response = res;
+        done();
+      });
+    });
+
+    it(`should return a status code 200`, (done) => {
+      const expectedValue = 200;
+      expect(response.statusCode).to.be.equal(200);
+      done();      
+    });
+
+    it(`should return an object`, (done) => {
+      const expectedValue = 'object';
+      expect(response.body).to.be.an('object');
+      done();      
+    });
+
+    it(`should contains a ticketData key`, (done) => {
+      const expectedValue = 'ticketData';
+      expect(response.body).to.have.property(expectedValue);
+      done();      
+    });
+
+    it(`should contains a value key`, (done) => {
+      const expectedValue = 'value';
+      expect(response.body.ticketData).to.have.property(expectedValue);
+      done();      
+    });
+
+    it(`should contains a dueDate key`, (done) => {
+      const expectedValue = 'dueDate';
+      expect(response.body.ticketData).to.have.property(expectedValue);
+      done();      
+    });
+
+    it(`should contains a dueDate key`, (done) => {
+      const expectedValue = 'barCode';
+      expect(response.body.ticketData).to.have.property(expectedValue);
+      done();      
+    });
   });
 });
